3b59668f8f18064e2a080c10e9f947dd06089cbe
idwong2000 <141781475+idwong2000@users.noreply.github.com>
2023-08-09 21:06:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,50 @@
 // create web server
-const express = require('express');
-const app = express();
+// listen to port 3000
+// when request comes in, read the file
+// send the content of the file
+// if error, send 404
+// if success, send content of the file
+
+// 1. require http module
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 const port = 3000;
 
-// create middleware
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-
-// set up middleware
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-
-// set up view engine
-app.set('view engine', 'ejs');
-
-// set up routes
-const commentsRoutes = require('./routes/comments');
-app.use('/comments', commentsRoutes);
-
-// root route
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-// error route
-app.get('*', (req, res) => {
-    res.render('error');
-});
-
-// listen for requests
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// 2. create server
+const server = http.createServer((req, res) => {
+    const pathName = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
+    if (pathName === '/comments' && req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(JSON.stringify(query));
+    } else if (pathName === '/form' && req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        fs.readFile(`${__dirname}/form.html`, 'utf-8', (err, data) => {
+            res.end(data);
+        });
+    } else if (pathName === '/form' && req.method === 'POST') {
+        let body = '';
+        req.on('data', (data) => {
+            body += data;
+        });
+        req.on('end', () => {
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+            res.end(`
+                <h1>Thanks for your feedback</h1>
+                <p>${body}</p>
+            `);
+        });
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 });
